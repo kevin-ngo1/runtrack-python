70f84753ebb630800166d7636f743c150eaa237a
kevin-ngo1 <miniguns@MiniGuns>
2023-11-24 17:01:46 +0100

    Add Input

--- jour05/job05/main.py
+++ jour05/job05/main.py
@@ -1,3 +1,4 @@
+#sans input
 def code(message, decale):
     alphabet="abcdefghijklmnopqrstuvwxyz"
     res=""
@@ -12,5 +13,24 @@ def code(message, decale):
                     res += alphabet[j+decale]
     print(res)
 
-code("je suis maya the bee zzzzzz", -2)
+code("je suis maya the bee zzzzzzyyyzzzz", 2)
+
+#avec input
+def code(message, decale):
+    alphabet="abcdefghijklmnopqrstuvwxyz"
+    res=""
+    tmp = 0
+    for lettre in message :
+        for j in range(len(alphabet)) :
+            if lettre == alphabet[j] :
+                if j+decale > 25 :
+                    tmp = j+decale - 26
+                    res += alphabet[tmp]
+                else :
+                    res += alphabet[j+decale]
+    print(res)
+
+message = input("Entrez votre message qui sera chiffré (tout en minuscule) :")
+decale = int(input("Entrez le nombre de décalage :"))
+code(message, decale)
 
